22393f7a37cecfad588b2a9b26e192b771edc1ef
JWilliams-Keyin <Jack.Williams@keyin.com>
2024-07-04 12:37:48 -0230

    Create math_using_input.py

---
+++ OldHSProjects/math_using_input.py
@@ -0,0 +1,8 @@
+#In this calculator, the user is prompted to enter their numbers
+#Each variable is set to the numbers that they input
+num1 = input('Enter first number:    ')
+num2 = input('Enter second number:    ')
+#Each variable is the added together and displayed through the "answer" variable
+answer = float(num1) + float(num2)
+#The answer is then shown to the user using the "print" function
+print('The sum of', num1, 'and', num2, 'is', answer)
