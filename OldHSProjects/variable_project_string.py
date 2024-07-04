0dd4f709e333b987a2e1951913b63bbbb6f954ef
JWilliams-Keyin <Jack.Williams@keyin.com>
2024-07-04 12:37:28 -0230

    Create variable_project_string.py

---
+++ OldHSProjects/variable_project_string.py
@@ -0,0 +1,9 @@
+#This program shows the song, band, and people that the user inputs in order to credit them
+#These variables are set to the user's answers to the prompts
+song = input('What is the name of the song? ')
+band = input('What is the name of the band? ')
+people = input('Who is in the band? ')
+#These strings then show the user their answers uppercase and title form
+print(song.upper())
+print(band.title())
+print(people.title())
