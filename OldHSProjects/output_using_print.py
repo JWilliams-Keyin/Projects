79ace28961ec9bf6a48955e2bd220af3cda43601
JWilliams-Keyin <Jack.Williams@keyin.com>
2024-07-04 12:37:42 -0230

    Create output_using_print.py

---
+++ OldHSProjects/output_using_print.py
@@ -0,0 +1,6 @@
+#this code says hello world
+print('hello, world')
+#the following code lists integers
+print(5, 4, 3, 2, 1)
+#this code says two integers with a word in between them
+print(5,'and', 4)
